60cc3bd285bab01a7ff7f318029d556c1e6e3928
David C <dchung20315@gmail.com>
2023-05-24 10:28:19 -0700

    added code to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -11,17 +11,59 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+     new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'WebPack Plugin',
+     }),
+
+     new GenerateSW(),
+     new WebpackPwaManifest({
+      name: 'Contact Cards',
+      short_name: 'Cards',
+      description: 'Cards to manage and maintain contacts contacts',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      start_url: './',
+      publicPath: '/',
+
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+     })
     ],
 
     // TODO: Add the correct modules
     module: {
-
-    }
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+          }
+          }
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        }
+      ]
+      },
   };
 };
