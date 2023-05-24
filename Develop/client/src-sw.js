3cf84718ff0237ff49e2b19ba5137e0413d7f46c
David C <dchung20315@gmail.com>
2023-05-24 10:32:53 -0700

    added serviceworker logic

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,1 +1,12 @@
 // TODO: Create a service worker that caches static assets:
+import { workbox } from workbox-window;
+
+export const registerServiceWorker = async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./src-sw.js');
+        } catch (e) {
+            console.log(`SW registration failed`);
+        }
+    }
+}
